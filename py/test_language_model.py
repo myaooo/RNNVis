47810bfbd46934365416a0962bd44e37877b8596
Ming Yao <yaoming.thu@gmail.com>
2017-02-05 10:51:51 +0800

    Update optimizer scheme and tests

--- py/test_language_model.py
+++ py/test_language_model.py
@@ -8,7 +8,9 @@ $ tar xvf simple-examples.tgz
 from py.datasets.data_utils import InputProducer
 from py.datasets.ptb_reader import ptb_raw_data
 from py.procedures import init_tf_environ
-import py.rnn as rnn
+import py.rnn.rnn as rnn
+import py.rnn.trainer as trainer
+import py.rnn.config_utils as config
 import tensorflow as tf
 
 flags = tf.flags
@@ -60,16 +62,16 @@ if __name__ == '__main__':
     train_inputs, train_targets, epoch_size = test_data_producer(train_data, batch_size, train_steps)
     valid_inputs, valid_targets, valid_epoch_size = test_data_producer(valid_data, batch_size, train_steps)
 
-    model = rnn.RNN('LSTM', rnn.get_initializer('random_uniform', minval=-0.1, maxval=0.1), "models/LSTM")
+    model = rnn.RNN('LSTM', config.get_initializer('random_uniform', minval=-0.1, maxval=0.1), "models/LSTM")
     model.add_cell(rnn.BasicLSTMCell, num_units=200)
     model.add_cell(rnn.BasicLSTMCell, num_units=200)
     model.set_input([None], tf.int32, 10000, 200)
-    model.set_output([None, 10000], data_type())
+    model.set_output([None, 10000], tf.float32)
     model.set_target([None], tf.int32)
-    model.set_loss_func(rnn.get_loss_func("sequence_loss"))
+    model.set_loss_func(config.get_loss_func("sequence_loss"))
     model.compile()
-    model.add_trainer(batch_size, train_steps, keep_prob, rnn.trainer.get_optimizer("GradientDescent"), test_lr_decay,
-                      rnn.trainer.get_gradient_clipper("global_norm", clip_norm=5.0))
+    model.add_trainer(batch_size, train_steps, keep_prob, trainer.get_optimizer("GradientDescent"), test_lr_decay,
+                      trainer.get_gradient_clipper("global_norm", clip_norm=5.0))
     model.add_validator(batch_size, train_steps)
     print('Start Training')
     model.train(train_inputs, train_targets, epoch_size, epoch_num,
