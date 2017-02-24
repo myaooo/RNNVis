19cdfa6d27e415f3540f455d7e43eb9179ed9a79
shaozu <shaozu.cao@gmail.com>
2017-02-24 18:54:43 +0800

    debugging

--- rnnvis/datasets/data_utils.py
+++ rnnvis/datasets/data_utils.py
@@ -331,5 +331,11 @@ def get_lm_data_producer(data, batch_size, num_steps, transpose=False):
 def get_sp_data_producer(data, label, batch_size, max_length, num_steps=None, transpose=False):
     s_producer = SentenceProducer(data, batch_size, max_length, num_steps)
     inputs = s_producer.get_feeder(transpose=transpose)
+
+    # normalize label to start from 0.
+    min_label = min(label)
+    label = label - min_label
+
     targets = ListFeeder(label[:s_producer.sentence_num], batch_size, inputs.sentence_size)
+
     return inputs, targets, targets.epoch_size
